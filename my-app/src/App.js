492e1a4ab2cf5dddbab4e65747d7ecd3500cd505
Nikki Lin <nikkiylin@gmail.com>
2020-03-10 16:00:28 -0700

    have my own profile dispaly on top.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,85 +2,40 @@ import React from 'react';
 
 import './App.css';
 import axios from "axios";
-import PersonCard from "./PersonCard";
+import Profile from "./Profile";
 
 class App extends React.Component {
   state = {
-    people: [],
-    peopleText: ''
+    profile: {},
+    followers: [],
   };
 
   componentDidMount() {
     axios
+    .get('https://api.github.com/users/XueYingLin')
+    .then(res => {
+      this.setState({
+        profile: res.data
+      });
+    })
+    .catch(err => console.log(err.message));
+    axios
     .get('https://api.github.com/users/XueYingLin/followers')
     .then(res => {
       this.setState({
-        people: res.data
+        followers: res.data
       });
     })
     .catch(err => console.log(err.message));
   }
   
-  handleChanges = e => {
-    this.setState({
-      peopleText: e.target.value
-    });
-  };
-
-  fetchPeople = e => {
-    e.preventDefault();
-    axios
-      .get(`https://api.github.com/users/XueYingLin/${this.state.peopleText}`)
-      .then(res => {
-        this.setState({
-          people: res.data
-        });
-      })
-      .catch(err => console.log(err.message));
-
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.people !== this.state.people) {
-      console.log("new people array");
-      axios
-      .get('https://api.github.com/users/XueYingLin/followers')
-      .then(res => {
-        this.setState({
-          people: res.data
-        });
-      })
-      .catch(err => console.log(err.message));
-      
-    }
-  }
-  
   render() {
-  return (
-    <div className="App">
-      <h1>My App</h1>
-      <div>
-        <input value={this.state.peopleText} onChange={this.handleChanges} />
-        <button onClick={this.fetchPeople}>Fetch People</button>
-        <div className="people">
-          {this.state.people.map(p => (
-            <PersonCard key={p}
-                        avatar_url={p.avatar_url}
-                       login= {p.login}
-                       html_url={p.html_url}
-                        />
-            
-          ))}
-
-{/* <img src={props.avatar_url} />
-            <p>{props.login}</p>
-            <p>{props.html_url}</p> */}
-
-        </div>
-      </div>
-      
-    </div>
-  );
+    return (
+      <Profile avatar_url={this.state.profile.avatar_url} 
+              login={this.state.profile.login} 
+              html_url={this.state.profile.html_url} 
+              followers={this.state.followers} />
+    );
   }
 }
 
---
+++ my-app/src/Profile.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import PersonCard from "./PersonCard";
+
+const Profile = (props) => {
+    return (
+        <div>
+            <div className="profile">
+                <img src={props.avatar_url} />
+                <p>{props.login}</p>
+                <p>{props.html_url}</p>
+            </div>
+            <p className="follower-string">Followers:</p>
+            
+            {props.followers.map(p => (
+                <PersonCard key={p}
+                            avatar_url={p.avatar_url}
+                            login= {p.login}
+                            html_url={p.html_url}
+                            />
+                
+          ))}
+    </div>);
+}
+
+export default Profile;
